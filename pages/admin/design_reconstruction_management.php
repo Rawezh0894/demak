90dde1aa87b32b69fe65c2f2a81aaeb597373d39
Rawezh0894 <rawezh.soft94@gmail.com>
2025-11-15 16:24:04 +0300

    گۆڕانکاریە نوێیەکان

--- pages/admin/design_reconstruction_management.php
+++ pages/admin/design_reconstruction_management.php
@@ -599,6 +599,7 @@ require_once '../../process/design-reconstruction/design-reconstruction.php';
                                        name="project_price" 
                                        id="projectPrice"
                                        required
+                                       placeholder="مثال: 100,000 دینار"
                                        class="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent bg-white dark:bg-gray-700 text-gray-900 dark:text-white">
                             </div>
                             
@@ -611,6 +612,7 @@ require_once '../../process/design-reconstruction/design-reconstruction.php';
                                        name="project_duration" 
                                        id="projectDuration"
                                        required
+                                       placeholder="مثال: 30 ڕۆژ یان 1 مانگ"
                                        class="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent bg-white dark:bg-gray-700 text-gray-900 dark:text-white">
                             </div>
                             
